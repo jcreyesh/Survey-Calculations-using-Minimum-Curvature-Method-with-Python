fe762bb04cc73d7b12e4fdd2f0ecf46277072518
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-17 15:20:56 -0600

    Update Well_Trajectory_App.py

--- Well_Trajectory_App.py
+++ Well_Trajectory_App.py
@@ -138,6 +138,7 @@ fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
 sl.plotly_chart(fig)
 
 # Exporting the dataframe
-sl.download_button(label="Download CSV", data=df)
+sl.write(df)
+#sl.download_button(label="Download CSV", data=df)
 
 
