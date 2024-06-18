12c0fe6f07f3aa344027af541c6820c3c428e3fb
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-17 20:49:01 -0600

    Update Well_Trajectory_App.py

--- Well_Trajectory_App.py
+++ Well_Trajectory_App.py
@@ -11,8 +11,8 @@ sl.image("wt_app_2.jpg")
 
 # Sidebar
 sl.title("Well Trajectory Calculations App - Minimum Curvature Method")
-sl.text("This simple App helps you to obtain a complete wellbore-trajectory using as input \ndata the measure depth(md), inclination(°),\
-azimuth(°) & the Vertical Section plane(°).\nThe App computes columns such as TVD, Northing, Easting, Vertical Section & DLS.\nDisplays the Vs,\
+sl.text("This simple App helps you to obtain a complete wellbore-trajectory using as input \ndata the measure depth, inclination,\
+azimuth & the Vertical Section plane.\nThe App computes columns such as TVD, Northing, Easting, Vertical Section & DLS.\nDisplays the Vs,\
 Northing vs Easting, inclination & DLS plots, as well as the 3D plot.\nFinally, you can retrieve the complete trajectory as a csv file by clicking\n\
 the Download button.")
 
