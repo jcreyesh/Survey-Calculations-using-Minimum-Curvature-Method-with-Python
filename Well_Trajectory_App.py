aa1c75708fd99147a773f6c7757a8ad13ee49925
Jose Carlos Reyes <153877979+jc-charlieman@users.noreply.github.com>
2024-06-20 16:51:16 -0600

    Update Well_Trajectory_App.py

--- Well_Trajectory_App.py
+++ Well_Trajectory_App.py
@@ -26,11 +26,11 @@ the Download button.\nCreated by José Carlos Reyes.")
  
  col1, col2, col3 = sl.columns([1, 1, 1])
  with col1: 
-     Well = sl.text_input("Well:", placeholder="type a name.")
+     Well = sl.text_input("Well:", placeholder="type a name")
  with col2:
-     Vs_plane = sl.number_input("Vertical Section plane Azimuth:", placeholder="type a vertical section azimuth.")
+     Vs_plane = sl.number_input("Vertical Section plane Azimuth:")
  with col3:
-   total_depth = sl.number_input("Total depth - TVD:", value=max(df["MD"]), placeholder="type a maximum vertical depth.") 
+   total_depth = sl.number_input("Total depth - TVD:", value=max(df["MD"])) 
  
  # Empty containers
  DLS, TVD, NS, EW, Vs = np.array([0]), np.array([0]), np.array([0]), np.array([0]), np.array([0])
