2de7316ddf23059e96108dcb86296da631850e9d
Jose Carlos Reyes <153877979+jcreyesh@users.noreply.github.com>
2024-09-05 12:00:49 -0600

    Update Well_Trajectory_App.py

--- Well_Trajectory_App.py
+++ Well_Trajectory_App.py
@@ -15,7 +15,7 @@ try:
  sl.text("This simple App helps you to obtain a complete wellbore-trajectory using as input \ndata the measure depth, inclination,\
   azimuth & the Vertical Section plane.The App\ncomputes columns such as TVD, Northing, Easting, Vertical Section & DLS. Displays \nthe Vs,\
   Northing vs Easting, inclination & DLS plots, as well as the 3D plot.\nFinally, you can retrieve the complete trajectory as a csv file by clicking\n\
-the Download button.\nCreated by José Carlos Reyes & Dr. Mario Alberto Vásquez.")
+the Download button.\nCreated by José Carlos Reyes & Mario Alberto Vásquez.")
  
  #sl.divider()
  
