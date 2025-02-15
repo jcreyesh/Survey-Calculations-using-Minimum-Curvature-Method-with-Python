ffcc1895b81e362656e996c10639f6283b8c9e1c
Jose Carlos Reyes <153877979+jcreyesh@users.noreply.github.com>
2025-02-14 20:17:17 -0600

    Update Well_Trajectory_App.py

--- Well_Trajectory_App.py
+++ Well_Trajectory_App.py
@@ -182,7 +182,7 @@ the Download button.\nCreated by José Carlos Reyes & Mario Alberto Vásquez.")
  df = df.apply(lambda col: round(col, 4))
                
  # Download Button
- # @sl.cache_resource
+ @sl.cache_data
  def convert_csv(df):
      return df.to_csv(index=False).encode("utf-8")
  
